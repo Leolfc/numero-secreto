7b3f6804fc4aae00862c5948ae5e57eb9a331772
Leolfc <147149946+Leolfc@users.noreply.github.com>
2025-02-18 10:21:27 -0300

    resolvendo merge

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 let listaDeNumerosSorteados = [];
-<<<<<<< HEAD
 let numeroLimite = 10;
-=======
-let numeroLimite = 50;
->>>>>>> 7fc07949785f9a8cb8fe2521cf5de1c642f6d3f4
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
